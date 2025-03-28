5647697e8cdc70de3e437e34090985288200b670
gabrielgcmr <gabrielcactus@gmail.com>
2025-03-28 16:30:29 -0300

    iniciando backend.

---
+++ backend/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func saud() {
+	fmt.Println("Mylons")
+}
+
+func main() {
+	saud()
+}
--- frontend/src/components/MainPageSections/5-SummarySection.tsx
+++ frontend/src/components/MainPageSections/5-SummarySection.tsx
@@ -1,5 +1,5 @@
-import usePatient  from "../../hooks/usePatient";
-import calculateCKDEPIIndex  from "../../services/clinicalCalculations/CKD-EPI/CKDEPIIndex";
+import usePatient from "../../hooks/usePatient";
+import calculateCKDEPIIndex from "../../services/clinicalCalculations/CKD-EPI/CKDEPIIndex";
 import calculateCVRIndex from "../../services/clinicalCalculations/CVR/CVRIndex";
 import calculateFIB4Index from "../../services/clinicalCalculations/FIB-4/FIB4Index";
 import CKDIndex from "../../services/problems/CKD/CKDIndex";
@@ -12,30 +12,50 @@ function SummarySection() {
   const { patientData } = usePatient();
 
   // Executa os cálculos diretamente
-  const { CVRRealRisk, CVRIdealRisk, CVRcategory } = calculateCVRIndex(patientData);
+  const { CVRRealRisk, CVRIdealRisk, CVRcategory } =
+    calculateCVRIndex(patientData);
   const { eGFR } = calculateCKDEPIIndex(patientData);
   const { fib4, fib4category } = calculateFIB4Index(patientData);
 
   return (
     <div className="p-4 bg-zinc-700 rounded-lg shadow-md mb-2">
-      <h3 className="text-base font-bold mb-2">🛑LISTA DE PROBLEMAS</h3>
+      <p className="text-base font-bold mb-2">
+        🛑<strong>LISTA DE PROBLEMAS</strong>
+      </p>
       {/* Exibe problemas de saúde */}
-      <HypertensionIndex/>
-      <DiabetesIndex/>
-      <TabagismIndex/>
-      <CKDIndex/>
-      <NAFLDIndex/>
+      <HypertensionIndex />
+      <DiabetesIndex />
+      <TabagismIndex />
+      <CKDIndex />
+      <NAFLDIndex />
 
       <p className="mt-2"></p>
-      <h3 className="text-base font-bold mb-2">🟢PREVENÇÕES E SEGMENTOS</h3>
+      <p className="text-base font-bold mb-2">
+        🟢<strong>PREVENÇÕES E SEGUIMENTOS</strong>
+      </p>
       {/* Exibe o risco cardiovascular */}
-      {CVRRealRisk !== undefined && <li> <strong>RCV:</strong> Risco Atual: {CVRRealRisk.toFixed(2)}% - ({CVRcategory}) || Risco Ideal: {CVRIdealRisk.toFixed(2)}% </li>}
+      {CVRRealRisk !== undefined && (
+        <li>
+          {" "}
+          <strong>RCV:</strong> Risco Atual: {CVRRealRisk.toFixed(2)}% - (
+          {CVRcategory}) || Risco Ideal: {CVRIdealRisk.toFixed(2)}%{" "}
+        </li>
+      )}
       {/* Exibe a TFG se houver */}
-      {eGFR !== undefined && <li><strong>TFG: </strong> {eGFR} mL/min/1.73m²</li>}
+      {eGFR !== undefined && (
+        <li>
+          <strong>TFG: </strong> {eGFR} mL/min/1.73m²
+        </li>
+      )}
       {/* Exibe o FIB-4 se houver */}
-      {fib4 !== undefined && <li><strong>FIB4: </strong>{fib4} pontos - Estadiamento de Fibrose estimado: {fib4category} </li>}
+      {fib4 !== undefined && (
+        <li>
+          <strong>FIB4: </strong>
+          {fib4} pontos - Estadiamento de Fibrose estimado: {fib4category}{" "}
+        </li>
+      )}
     </div>
   );
 }
 
-export default SummarySection
+export default SummarySection;
